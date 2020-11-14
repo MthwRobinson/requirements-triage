fe5ce6d4cd74ca9aecb20e52e12efa27e26f4779
Matt Robinson <mthw.wm.robinson@gmail.com>
2020-11-13 22:42:29 -0500

    added script for training and saving models

--- reqs_triage/classifer.py
+++ reqs_triage/classifer.py
@@ -1,13 +1,35 @@
 import os
+import pickle
 
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 from transformers import pipeline
 import tqdm
+
+from reqs_triage.dataset import read_dataset
 
 
 DATA_DIR = "/home/matt/data/ghub_labels"
 MODEL_DIR = "/home/matt/models/feature-request"
+
+
+MODELS = [
+    ("rf-tfidf", RandomForestClassifier(), TfidfVectorizer()),
+    ("rf-bow", RandomForestClassifier(), CountVectorizer()),
+    ("lr-tfidf", LogisticRegression(), TfidfVectorizer()),
+    ("lf-bow", LogisticRegression(), CountVectorizer()),
+    ("nb-tfidf", BernoulliNB(), TfidfVectorizer()),
+    ("nb-bow", BernoulliNB(), CountVectorizer()),
+    ("knn-tfidf", KNeighborsClassifier(), TfidfVectorizer()),
+    ("knn-bow", KNeighborsClassifier(), CountVectorizer()),
+    ("svm-tfidf", SVC(), TfidfVectorizer()),
+    ("svm-bow", SVC(), CountVectorizer()),
+]
 
 
 def train_model(classifier, vectorizer, data):
@@ -34,6 +56,19 @@ def train_model(classifier, vectorizer, data):
     y = np.array(data["target"] == "feature").astype(int)
     classifier.fit(X, y)
     return classifier, vectorizer
+
+
+def train_and_save_models():
+    """Trains all of the models in the list and serializes them to disk."""
+    train = read_dataset(DATA_DIR, "train")
+    for label, classifier, vectorizer in tqdm.tqdm(MODELS):
+        classifier, vectorizer = train_model(classifier, vectorizer, train)
+
+        with open(os.path.join(MODEL_DIR, f"{label}-classifier.pkl"), "wb") as f:
+            pickle.dump(classifier, f)
+
+        with open(os.path.join(MODEL_DIR, f"{label}-vectorizer.pkl"), "wb") as f:
+            pickle.dump(vectorizer, f)
 
 
 def t5_inference(data):
