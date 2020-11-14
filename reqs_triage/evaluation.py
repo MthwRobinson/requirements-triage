c5a25a3aa0c81c3614a5968546b9d544893383c4
Matt Robinson <mthw.wm.robinson@gmail.com>
2020-11-14 12:05:32 -0500

    included bootstrap evaluate

--- reqs_triage/evaluation.py
+++ reqs_triage/evaluation.py
@@ -10,6 +10,34 @@ METRICS = {
 }
 
 
+def bootstrap_evaluation(data, sample_size=250, iterations=100):
+    """Evaluates the model outputs on a random subsample of the test data.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        A dataframe containing the model outputs
+    sample_size : int
+        The samples size for the evaluation run
+    iterations : int
+        The number of iterations to run
+
+    Returns
+    -------
+    performance : pd.DataFrame
+        A dataframe containing the performance metrics for each model and an id column
+        for each bootstrap iteration
+    """
+    performance = []
+    eval_data = data.copy()
+    for iteration in range(iterations):
+        eval_data = eval_data.sample(frac=1).reset_index(drop=True)
+        iter_performance = performance_metrics(eval_data[:sample_size])
+        iter_performance["iteration"] = iteration
+        performance.append(iter_performance)
+    return pd.concat(performance)
+
+
 def performance_metrics(data):
     """Measures performance based on model outputs
 
